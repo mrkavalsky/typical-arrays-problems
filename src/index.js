58c1b5ebece8261461d830509a23d4e35659f876
mrkavalsky <70824301+mrkavalsky@users.noreply.github.com>
2020-10-02 16:16:44 +0300

    feat: min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if(array === undefined || !Array.isArray(array) || array.length == 0) return 0;
+  let min = 0;
+  for(let i of array){
+    if(min > i) min = i;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || !Array.isArray(array) || array.length == 0) return 0;
+  let max = array[1];
+  for(let i of array){
+    if(max < i) max = i;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || !Array.isArray(array) || array.length == 0) return 0;
+  let avg = 0;
+  for(let i of array){
+    avg += i;
+  }
+  avg /= array.length;
+  return avg;
 }
